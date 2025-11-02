47cce6e632b3fb46a346d6fb9b474608654580f6
zoe-704 <sunzoey@gmail.com>
2025-11-02 12:54:14 -0800

    V1

--- src/Finder.java
+++ src/Finder.java
@@ -14,6 +14,18 @@ public class Finder {
     private static final String INVALID = "INVALID KEY";
     public Finder() {}
 
+    static final int RADIX = 256;
+    static final long P = 543_211_019;
+
+    // Creates hash using horner's method
+    public static long hash(String s, int start, int n) {
+        long h = 0;
+        for (int i = start; i < start + n; i++) {
+            h = (h * RADIX + s.charAt(i)) % P;
+        }
+        return h;
+    }
+
     public void buildTable(BufferedReader br, int keyCol, int valCol) throws IOException {
         // TODO: Complete the buildTable() function!
         String line = br.readLine();
