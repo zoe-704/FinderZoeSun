9da1ec1654a9d0b1b0cf7510f8a0845df64cba97
zoe-704 <sunzoey@gmail.com>
2025-11-03 17:05:27 -0800

    V1 work

--- src/Finder.java
+++ src/Finder.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.ArrayList;
 
 /**
  * Finder
@@ -15,28 +16,54 @@ public class Finder {
     public Finder() {}
 
     static final int RADIX = 256;
-    static final long P = 543_211_019;
+    static final int P = 524287;
+    static final int MAX_N = 1000000;
+    String k, v;
 
-    // Creates hash using horner's method
-    public static long hash(String s, int start, int n) {
-        long h = 0;
-        for (int i = start; i < start + n; i++) {
-            h = (h * RADIX + s.charAt(i)) % P;
+
+
+    public class Hash {
+        private static int bucket;
+        private ArrayList<String[]> table;
+        public Hash(int bucket) {
+            this.bucket = bucket;
+            this.table = new ArrayList<String[bucket];
+            for (int i = 0; i < table.size(); i++) {
+                table[i] = new ArrayList<String>();
+            }
         }
-        return h;
-    }
+        // Creates hash using horner's method
+        public static int hashKey(String s) {
+            int h = 0;
+            for (int i = 0; i < s.length(); i++) {
+                h = (h * RADIX + s.charAt(i)) % P;
+            }
+            return h % bucket;
+        }
 
+        public void insert(String key, String value) {
+            int keyHash = hashKey(key);
+            table[keyHash].append(value);
+        }
+    }
+    // creating the data structure
     public void buildTable(BufferedReader br, int keyCol, int valCol) throws IOException {
         // TODO: Complete the buildTable() function!
         String line = br.readLine();
         String[] arr = line.split(",");
-        String key = arr[keyCol];
-        String value = arr[valCol];
+        k = arr[keyCol];
+        v = arr[valCol];
+        insert(k, v);
         br.close();
     }
-
+    // getting the value from the data structure given the key
     public String query(String key){
         // TODO: Complete the query() function!
+        int keyLength = key.length() % MAX_N;
+
+        String[] queryTable = new String[MAX_N];
+        queryTable[keyHash] = v;
+
         return INVALID;
     }
 }
