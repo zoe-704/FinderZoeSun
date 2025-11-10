027498ce10cf75c3397a779fb2f68ae9f6137df2
zoe-704 <sunzoey@gmail.com>
2025-11-09 19:45:05 -0800

    Final Finder work

--- src/Finder.java
+++ src/Finder.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Finder
@@ -12,58 +13,25 @@ import java.util.ArrayList;
 
 public class Finder {
 
-    private static final String INVALID = "INVALID KEY";
-    public Finder() {}
-
-    static final int RADIX = 256;
-    static final int P = 524287;
-    static final int MAX_N = 1000000;
-    String k, v;
-
-
-
-    public class Hash {
-        private static int bucket;
-        private ArrayList<String[]> table;
-        public Hash(int bucket) {
-            this.bucket = bucket;
-            this.table = new ArrayList<String[bucket];
-            for (int i = 0; i < table.size(); i++) {
-                table[i] = new ArrayList<String>();
-            }
-        }
-        // Creates hash using horner's method
-        public static int hashKey(String s) {
-            int h = 0;
-            for (int i = 0; i < s.length(); i++) {
-                h = (h * RADIX + s.charAt(i)) % P;
-            }
-            return h % bucket;
-        }
-
-        public void insert(String key, String value) {
-            int keyHash = hashKey(key);
-            table[keyHash].append(value);
-        }
+    private static HashMap hashMap;
+    public Finder() {
+        hashMap = new HashMap();
     }
+
     // creating the data structure
     public void buildTable(BufferedReader br, int keyCol, int valCol) throws IOException {
-        // TODO: Complete the buildTable() function!
-        String line = br.readLine();
-        String[] arr = line.split(",");
-        k = arr[keyCol];
-        v = arr[valCol];
-        insert(k, v);
+        String line;
+        while ((line = br.readLine()) != null) {
+            // Process data in each line
+            String[] arr = line.split(",");
+            String key = arr[keyCol];
+            String value = arr[valCol];
+            hashMap.insert(key, value);
+        }
         br.close();
     }
     // getting the value from the data structure given the key
     public String query(String key){
-        // TODO: Complete the query() function!
-        int keyLength = key.length() % MAX_N;
-
-        String[] queryTable = new String[MAX_N];
-        queryTable[keyHash] = v;
-
-        return INVALID;
+        return hashMap.findValue(key);
     }
 }
---
+++ src/Pair.java
@@ -0,0 +1,16 @@
+public class Pair {
+    private String key;
+    private String value;
+    // Stores key-value pair in a HashMap
+    public Pair(String key, String value) {
+        this.key = key;
+        this.value = value;
+    }
+    // Setters and getters
+    public String getKey() {
+        return key;
+    }
+    public String getValue() {
+        return value;
+    }
+}
